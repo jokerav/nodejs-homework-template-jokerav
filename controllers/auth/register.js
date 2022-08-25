dd559860c66c06c0e6f44bbe5d4ad436491bb865
jokerav <50701756+jokerav@users.noreply.github.com>
2022-08-25 15:17:18 +0300

    Update register.js

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -7,14 +7,16 @@ const register = async (req, res) => {
   if (error) {
     throw createError(400, error.message);
   }
+  
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user) {
     throw createError(409, `${email} is already exist`);
   }
+  
   const hashPassword = await bcrypt.hash(password, 10);
   const result = await User.create({ ...req.body, password: hashPassword });
-  console.log(result);
+  
   res.status(201).json({
     user: {
       email,
