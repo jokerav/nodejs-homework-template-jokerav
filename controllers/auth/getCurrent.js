d86804505ee1efe4986beb729e55d81cc8bb6297
jokerav <50701756+jokerav@users.noreply.github.com>
2022-08-25 15:19:33 +0300

    Update getCurrent.js

--- controllers/auth/getCurrent.js
+++ controllers/auth/getCurrent.js
@@ -5,10 +5,10 @@ const { User } = require("../../models/user");
 const getCurrent = async (req, res) => {
   const { authorization = "" } = req.headers;
   const [, token] = authorization.split(" ");
-
   const { id } = jwt.verify(token, SECRET_KEY);
+  
   const { email, subscription } = await User.findById(id);
-
+  
   res.json({ email, subscription });
 };
 
