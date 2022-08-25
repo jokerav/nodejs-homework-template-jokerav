530d8c8dd3c9ae0875def0ab304882eb01c6e6f6
jokerav <50701756+jokerav@users.noreply.github.com>
2022-08-25 15:07:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const app = require("./app");
 require("dotenv").config();
 const { PORT, DB_HOST } = process.env;
+
 const connection = mongoose.connect(DB_HOST, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
