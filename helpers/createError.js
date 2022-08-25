a61a98bdd071ddc54a17baf2606e988e50d9d1e9
jokerav <50701756+jokerav@users.noreply.github.com>
2022-08-25 15:20:49 +0300

    Update createError.js

--- helpers/createError.js
+++ helpers/createError.js
@@ -4,6 +4,7 @@ const messages = {
   404: "Not found",
   409: "Conflict",
 };
+
 const createError = (status, message = messages[status]) => {
   const error = new Error(message);
   error.status = status;
