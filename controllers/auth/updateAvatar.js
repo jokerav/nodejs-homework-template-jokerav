be32d7230eabeab06197721e6dee6f3d5d651e5b
jokerav <jokeravdeveloper@gmail.com>
2022-09-01 16:58:51 +0300

    пережатие картинки

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,7 +1,8 @@
 const fs = require("fs/promises");
 const path = require("path");
-
+const Jimp = require("jimp");
 const { basedir } = global;
+const { createError } = require(`${basedir}/helpers`);
 
 const { User } = require(`${basedir}/models/user`);
 const avatarsDir = path.normalize(`${basedir}/public/avatars`);
@@ -13,8 +14,23 @@ const updateAvatar = async (req, res) => {
     const [extention] = filename.split(".").reverse();
     const avatarName = `${_id}.${extention}`;
     const resultUpload = path.join(avatarsDir, avatarName);
-    await fs.rename(tempUpload, resultUpload);
-    const avatarURL = path.join("avatars", resultUpload);
+
+    Jimp.read(`${tempUpload}`)
+      .then((img) => {
+        return (
+          img
+            .resize(250, 250)
+            // копируем картинку в папку /public/avatars
+            .write(resultUpload)
+        );
+      })
+      .catch(createError(500));
+    // удаляем оригинал
+    await fs.unlink(`${tempUpload}`);
+
+    // вот здесь я не уверен какой путь лучше сохранять в БД, абсолютный или относительный
+    const avatarURL = `../../public/avatars/${avatarName}`;
+
     await User.findByIdAndUpdate(_id, { avatarURL });
     res.json({
       avatarURL,
--- helpers/createError.js
+++ helpers/createError.js
@@ -3,6 +3,7 @@ const messages = {
   401: "Unauthorized",
   404: "Not found",
   409: "Conflict",
+  500: "Internal Server Error",
 };
 
 const createError = (status, message = messages[status]) => {
